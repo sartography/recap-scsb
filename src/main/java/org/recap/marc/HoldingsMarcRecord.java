85e633f841af075ed6f4cc56c9cbf883984e891b
chenchulakshmig <chenchulakshmi.g@htcindia.com>
2016-10-19 20:17:29 +0530

    Added Bib related pojos.

---
+++ src/main/java/org/recap/marc/HoldingsMarcRecord.java
@@ -0,0 +1,30 @@
+package org.recap.marc;
+
+import org.marc4j.marc.Record;
+import org.recap.marc.ItemMarcRecord;
+
+import java.util.List;
+
+/**
+ * Created by chenchulakshmig on 14/10/16.
+ */
+public class HoldingsMarcRecord {
+    Record holdingsRecord;
+    List<ItemMarcRecord> itemMarcRecordList;
+
+    public Record getHoldingsRecord() {
+        return holdingsRecord;
+    }
+
+    public void setHoldingsRecord(Record holdingsRecord) {
+        this.holdingsRecord = holdingsRecord;
+    }
+
+    public List<ItemMarcRecord> getItemMarcRecordList() {
+        return itemMarcRecordList;
+    }
+
+    public void setItemMarcRecordList(List<ItemMarcRecord> itemMarcRecordList) {
+        this.itemMarcRecordList = itemMarcRecordList;
+    }
+}
