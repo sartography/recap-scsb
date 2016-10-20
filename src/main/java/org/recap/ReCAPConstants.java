83f60b502755b07eafcc42aa72ff239cf741bc96
chenchulakshmig <chenchulakshmi.g@htcindia.com>
2016-10-20 17:05:29 +0530

    Added rest API for Accession.

--- src/main/java/org/recap/ReCAPConstants.java
+++ src/main/java/org/recap/ReCAPConstants.java
@@ -18,6 +18,22 @@ public class ReCAPConstants {
     public static final String REASON_FOR_FAILURE = "ReasonForFailure";
 
     public static final String ITEM_BARCDE_DOESNOT_EXIST = "Item Barcode doesn't exist in SCSB database.";
+    public static final String CUSTOMER_CODE_DOESNOT_EXIST = "Customer Code doesn't exist in SCSB database.";
     public static final String REQUESTED_ITEM_DEACCESSIONED = "The requested item has already been DeAccessioned.";
 
+    public static final String ERROR_DESCRIPTION = "ErrorDescription";
+    public static final String OWNING_INSTITUTION_HOLDINGS_ID = "OwningInstitutionHoldingsId";
+
+    public static final String LOCAL_ITEM_ID = "LocalItemId";
+    public static final String ITEM_BARCODE = "ItemBarcode";
+    public static final String CUSTOMER_CODE = "CustomerCode";
+    public static final String COLLECTION_GROUP_DESIGNATION = "CollectionGroupDesignation";
+    public static final String CREATE_DATE_ITEM = "CreateDateItem";
+    public static final String LAST_UPDATED_DATE_ITEM = "LastUpdatedDateItem";
+
+    public static final String COLUMBIA = "CUL";
+    public static final String PRINCETON = "PUL";
+    public static final String NYPL = "NYPL";
+
+    public static final String RESPONSE_DATE = "Date";
 }
