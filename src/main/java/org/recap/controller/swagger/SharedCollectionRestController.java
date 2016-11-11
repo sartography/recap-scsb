98a1d66f8b94bebdfd5512e06e6a94db418d99a1
angelindayana <angelin.dayana@gmail.com>
2016-11-11 16:45:02 +0530

    Removed the Accession, DeAccession implementations and kept only the api calls.

--- src/main/java/org/recap/controller/swagger/SharedCollectionRestController.java
+++ src/main/java/org/recap/controller/swagger/SharedCollectionRestController.java
@@ -4,21 +4,16 @@ import io.swagger.annotations.*;
 import org.apache.commons.lang3.StringUtils;
 import org.recap.ReCAPConstants;
 import org.recap.model.AccessionRequest;
-import org.recap.model.DeAccessionDBResponseEntity;
 import org.recap.model.DeAccessionRequest;
-import org.recap.util.AccessionService;
-import org.recap.util.DeAccessionUtil;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.*;
+import java.util.Date;
 
 /**
  * Created by chenchulakshmig on 6/10/16.
@@ -31,14 +26,8 @@ public class SharedCollectionRestController {
     @Value("${server.protocol}")
     String serverProtocol;
 
-    @Value("${scsb.persistence.url}")
-    String scsbPersistenceUrl;
-
-    @Autowired
-    DeAccessionUtil deAccessionUtil;
-
-    @Autowired
-    AccessionService accessionService;
+    @Value("${scsb.solr.client.url}")
+    String scsbSolrClientUrl;
 
     @RequestMapping(value = "/itemAvailabilityStatus", method = RequestMethod.GET)
     @ApiOperation(value = "itemAvailabilityStatus",
@@ -49,9 +38,10 @@ public class SharedCollectionRestController {
         RestTemplate restTemplate = new RestTemplate();
         String itemStatus = null;
         try {
-            itemStatus = restTemplate.getForObject(serverProtocol + scsbPersistenceUrl + "item/getItemStatusByBarcodeAndIsDeletedFalse?barcode=" + itemBarcode, String.class);
+            itemStatus = restTemplate
+                    .getForObject(serverProtocol+scsbSolrClientUrl+"/sharedCollection/itemAvailabilityStatus?itemBarcode="+itemBarcode,  String.class);
         } catch (Exception exception) {
-            ResponseEntity responseEntity = new ResponseEntity("Scsb Persistence Service is Unavailable.", getHttpHeaders(), HttpStatus.SERVICE_UNAVAILABLE);
+            ResponseEntity responseEntity = new ResponseEntity("Scsb Solr Client Service is Unavailable.", getHttpHeaders(), HttpStatus.SERVICE_UNAVAILABLE);
             return responseEntity;
         }
         if (StringUtils.isEmpty(itemStatus)) {
@@ -69,31 +59,15 @@ public class SharedCollectionRestController {
     @ApiResponses(value = {@ApiResponse(code = 200, message = "OK")})
     @ResponseBody
     public ResponseEntity deAccession(@ApiParam(value = "Item Barcodes with ',' separated", required = true, name = "itemBarcodes") @RequestBody DeAccessionRequest deAccessionRequest) {
-        List<DeAccessionDBResponseEntity> deAccessionDBResponseEntities = deAccessionUtil.deAccessionItemsInDB(deAccessionRequest.getItemBarcodes());
-        deAccessionUtil.processAndSave(deAccessionDBResponseEntities);
-        if (!CollectionUtils.isEmpty(deAccessionDBResponseEntities)) {
-            Map<String, String> resultMap = new HashMap<>();
-            List<Integer> bibIds = new ArrayList<>();
-            List<Integer> holdingsIds = new ArrayList<>();
-            List<Integer> itemIds = new ArrayList<>();
-            Map<String, Integer> ownInstAndItemIdMap = new HashMap<>();
-            for (DeAccessionDBResponseEntity deAccessionDBResponseEntity : deAccessionDBResponseEntities) {
-                if (deAccessionDBResponseEntity.getStatus().equalsIgnoreCase(ReCAPConstants.FAILURE)) {
-                    resultMap.put(deAccessionDBResponseEntity.getBarcode(), deAccessionDBResponseEntity.getStatus() + " - " + deAccessionDBResponseEntity.getReasonForFailure());
-                } else if (deAccessionDBResponseEntity.getStatus().equalsIgnoreCase(ReCAPConstants.SUCCESS)) {
-                    resultMap.put(deAccessionDBResponseEntity.getBarcode(), deAccessionDBResponseEntity.getStatus());
-                    bibIds.addAll(deAccessionDBResponseEntity.getBibliographicIds());
-                    holdingsIds.addAll(deAccessionDBResponseEntity.getHoldingIds());
-                    itemIds.add(deAccessionDBResponseEntity.getItemId());
-                    ownInstAndItemIdMap.put(deAccessionDBResponseEntity.getInstitutionCode(), deAccessionDBResponseEntity.getItemId());
-                }
-            }
-            deAccessionUtil.checkAndCancelHoldsIfExists(ownInstAndItemIdMap);
-            deAccessionUtil.deAccessionItemsInSolr(bibIds, holdingsIds, itemIds);
-            ResponseEntity responseEntity = new ResponseEntity(resultMap, getHttpHeaders(), HttpStatus.OK);
+        RestTemplate restTemplate = new RestTemplate();
+        try {
+            String response = restTemplate.postForObject(serverProtocol + scsbSolrClientUrl + "/sharedCollection/deAccession", deAccessionRequest, String.class);
+            ResponseEntity responseEntity = new ResponseEntity(response, getHttpHeaders(), HttpStatus.OK);
+            return responseEntity;
+        } catch (Exception ex) {
+            ResponseEntity responseEntity = new ResponseEntity("Scsb Solr Client Service is Unavailable.", getHttpHeaders(), HttpStatus.SERVICE_UNAVAILABLE);
             return responseEntity;
         }
-        return null;
     }
 
     @RequestMapping(value = "/accession", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
@@ -102,13 +76,13 @@ public class SharedCollectionRestController {
     @ApiResponses(value = {@ApiResponse(code = 200, message = "OK")})
     @ResponseBody
     public ResponseEntity accession(@ApiParam(value = "Item Barcode and Customer Code", required = true, name = "Item Barcode And Customer Code") @RequestBody AccessionRequest accessionRequest) {
-        String owningInstitution = accessionService.getOwningInstitution(accessionRequest.getCustomerCode());
-        if (StringUtils.isBlank(owningInstitution)) {
-            ResponseEntity responseEntity = new ResponseEntity(ReCAPConstants.CUSTOMER_CODE_DOESNOT_EXIST, getHttpHeaders(), HttpStatus.OK);
-            return responseEntity;
-        } else {
-            String response = accessionService.processRequest(accessionRequest.getItemBarcode(), owningInstitution);
+        RestTemplate restTemplate = new RestTemplate();
+        try {
+            String response = restTemplate.postForObject(serverProtocol + scsbSolrClientUrl + "sharedCollection/accession", accessionRequest, String.class);
             ResponseEntity responseEntity = new ResponseEntity(response, getHttpHeaders(), HttpStatus.OK);
+            return responseEntity;
+        } catch (Exception exception) {
+            ResponseEntity responseEntity = new ResponseEntity("Scsb Solr Client Service is Unavailable.", getHttpHeaders(), HttpStatus.SERVICE_UNAVAILABLE);
             return responseEntity;
         }
     }
--- src/test/java/org/recap/controller/swagger/SharedCollectionRestControllerUT.java
+++ src/test/java/org/recap/controller/swagger/SharedCollectionRestControllerUT.java
@@ -1,21 +1,16 @@
 package org.recap.controller.swagger;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 import org.recap.controller.BaseControllerUT;
-import org.recap.model.*;
+import org.recap.model.AccessionRequest;
+import org.recap.model.DeAccessionRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.web.client.RestTemplate;
 
-import java.io.File;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
@@ -32,23 +27,12 @@ public class SharedCollectionRestControllerUT extends BaseControllerUT {
     @Value("${server.protocol}")
     String serverProtocol;
 
-    @Value("${scsb.persistence.url}")
-    String scsbPersistenceUrl;
-
     @Value("${scsb.solr.client.url}")
     String scsbSolrClientUrl;
 
     @Test
     public void itemAvailabilityStatus() throws Exception {
-        Random random = new Random();
-        String itemBarcode = String.valueOf(random.nextInt());
-        BibliographicEntity bibliographicEntity = getBibEntityWithHoldingsAndItem(itemBarcode);
-
-        RestTemplate restTemplate = new RestTemplate();
-        String createUrl = serverProtocol + scsbPersistenceUrl + "bibliographic/create";
-        Integer bibliographicId = restTemplate.postForObject(createUrl, bibliographicEntity, Integer.class);
-        assertNotNull(bibliographicId);
-
+        String itemBarcode = "32101056185125";
         MvcResult savedResult = this.mockMvc.perform(get("/sharedCollection/itemAvailabilityStatus")
                 .headers(getHttpHeaders())
                 .param("itemBarcode", itemBarcode))
@@ -64,21 +48,6 @@ public class SharedCollectionRestControllerUT extends BaseControllerUT {
     public void deAccession() throws Exception {
         Random random = new Random();
         String itemBarcode = String.valueOf(random.nextInt());
-        BibliographicEntity bibliographicEntity = getBibEntityWithHoldingsAndItem(itemBarcode);
-
-        RestTemplate restTemplate = new RestTemplate();
-        String createUrl = serverProtocol + scsbPersistenceUrl + "bibliographic/create";
-        Integer bibliographicId = restTemplate.postForObject(createUrl, bibliographicEntity, Integer.class);
-        assertNotNull(bibliographicId);
-
-        BibliographicEntity savedBibliographicEntity = restTemplate.getForObject(serverProtocol + scsbPersistenceUrl + "bibliographic/search/findByBibliographicId?bibliographicId={bibliographicId}", BibliographicEntity.class, bibliographicId);
-        assertNotNull(savedBibliographicEntity);
-
-        restTemplate.postForLocation(serverProtocol + scsbSolrClientUrl + "/solrIndexer/indexByBibliographicId", bibliographicId);
-
-        Bib bib = restTemplate.getForObject(serverProtocol + scsbSolrClientUrl + "bibSolr/search/findByBibId?bibId=" + bibliographicId, Bib.class);
-        assertNotNull(bib);
-        assertEquals(bib.getBibId(), bibliographicId);
 
         DeAccessionRequest deAccessionRequest = new DeAccessionRequest();
         deAccessionRequest.setItemBarcodes(Arrays.asList(itemBarcode));
@@ -92,7 +61,7 @@ public class SharedCollectionRestControllerUT extends BaseControllerUT {
                 .andReturn();
 
         String result = mvcResult.getResponse().getContentAsString();
-        assertEquals(result, "{\"" + itemBarcode + "\":\"Success\"}");
+        assertNotNull(result);
     }
 
     @Test
@@ -112,65 +81,6 @@ public class SharedCollectionRestControllerUT extends BaseControllerUT {
 
         String result = mvcResult.getResponse().getContentAsString();
         assertEquals(result, "Success");
-    }
-
-    private BibliographicEntity getBibEntityWithHoldingsAndItem(String itemBarcode) throws Exception {
-        Random random = new Random();
-        File bibContentFile = getBibContentFile();
-        File holdingsContentFile = getHoldingsContentFile();
-        String sourceBibContent = FileUtils.readFileToString(bibContentFile, "UTF-8");
-        String sourceHoldingsContent = FileUtils.readFileToString(holdingsContentFile, "UTF-8");
-
-        BibliographicEntity bibliographicEntity = new BibliographicEntity();
-        bibliographicEntity.setContent(sourceBibContent.getBytes());
-        bibliographicEntity.setCreatedDate(new Date());
-        bibliographicEntity.setLastUpdatedDate(new Date());
-        bibliographicEntity.setCreatedBy("tst");
-        bibliographicEntity.setLastUpdatedBy("tst");
-        bibliographicEntity.setOwningInstitutionId(1);
-        bibliographicEntity.setOwningInstitutionBibId(String.valueOf(random.nextInt()));
-        bibliographicEntity.setDeleted(false);
-
-        HoldingsEntity holdingsEntity = new HoldingsEntity();
-        holdingsEntity.setContent(sourceHoldingsContent.getBytes());
-        holdingsEntity.setCreatedDate(new Date());
-        holdingsEntity.setLastUpdatedDate(new Date());
-        holdingsEntity.setCreatedBy("tst");
-        holdingsEntity.setLastUpdatedBy("tst");
-        holdingsEntity.setOwningInstitutionId(1);
-        holdingsEntity.setOwningInstitutionHoldingsId(String.valueOf(random.nextInt()));
-        holdingsEntity.setDeleted(false);
-
-        ItemEntity itemEntity = new ItemEntity();
-        itemEntity.setLastUpdatedDate(new Date());
-        itemEntity.setOwningInstitutionItemId(String.valueOf(random.nextInt()));
-        itemEntity.setOwningInstitutionId(1);
-        itemEntity.setBarcode(itemBarcode);
-        itemEntity.setCallNumber("x.12321");
-        itemEntity.setCollectionGroupId(1);
-        itemEntity.setCallNumberType("1");
-        itemEntity.setCustomerCode("123");
-        itemEntity.setCreatedDate(new Date());
-        itemEntity.setCreatedBy("tst");
-        itemEntity.setLastUpdatedBy("tst");
-        itemEntity.setItemAvailabilityStatusId(1);
-        itemEntity.setDeleted(false);
-
-        holdingsEntity.setItemEntities(Arrays.asList(itemEntity));
-        bibliographicEntity.setHoldingsEntities(Arrays.asList(holdingsEntity));
-        bibliographicEntity.setItemEntities(Arrays.asList(itemEntity));
-
-        return bibliographicEntity;
-    }
-
-    private File getBibContentFile() throws URISyntaxException {
-        URL resource = getClass().getResource("BibContent.xml");
-        return new File(resource.toURI());
-    }
-
-    private File getHoldingsContentFile() throws URISyntaxException {
-        URL resource = getClass().getResource("HoldingsContent.xml");
-        return new File(resource.toURI());
     }
 
     private HttpHeaders getHttpHeaders() {
