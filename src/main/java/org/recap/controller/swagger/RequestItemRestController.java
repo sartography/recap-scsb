3f19384b2e246f585e4e883e69c884c13810859b
sudhish74 <sudhish.kutty@htcindia.com>
2016-11-16 20:08:26 +0530

    Swagger calls for ESIP API calls

--- src/main/java/org/recap/controller/swagger/RequestItemRestController.java
+++ src/main/java/org/recap/controller/swagger/RequestItemRestController.java
@@ -1,12 +1,17 @@
 package org.recap.controller.swagger;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.*;
 import org.recap.ReCAPConstants;
 import org.recap.model.ItemRequestInformation;
+import org.recap.model.ItemResponseInformation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import java.util.Date;
 
@@ -17,7 +22,7 @@ import java.util.Date;
 @RequestMapping("/requestItem")
 @Api(value = "requestItem", description = "Request Item", position = 1)
 public class RequestItemRestController {
-
+    private Logger logger = LoggerFactory.getLogger(RequestItemRestController.class);
     @Value("${server.protocol}")
     String serverProtocol;
 
@@ -43,6 +48,110 @@ public class RequestItemRestController {
         return responseEntity;
     }
 
+    @RequestMapping(value = "/checkoutItemRequest" , method = RequestMethod.POST)
+    @ApiOperation(value = "checkoutItemRequest",
+            notes = "Checkout Item Request from Owning institution", nickname = "checkoutItemRequest")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "OK")})
+    @ResponseBody
+    public ItemResponseInformation checkoutItemRequest(@ApiParam(value = "Parameters for requesting an item" , required = true , name = "requestItemJson")@RequestBody ItemRequestInformation itemRequestInfo){
+        ItemResponseInformation itemResponseInformation= null;
+        String response = "";
+        RestTemplate restTemplate = new RestTemplate();
+        try {
+            response = restTemplate.postForEntity(serverProtocol + scsbCircUrl + "requestItem/checkoutItem", itemRequestInfo, String.class).getBody();
+            ObjectMapper om = new ObjectMapper();
+            itemResponseInformation = om.readValue(response, ItemResponseInformation.class);
+        }catch(RestClientException ex){
+            logger.error("RestClient : "+ ex.getMessage());
+        }catch(Exception ex){
+            logger.error("Exception : "+ex.getMessage());
+        }
+        return itemResponseInformation;
+    } //
+
+    @RequestMapping(value = "/checkinItem" , method = RequestMethod.POST)
+    @ApiOperation(value = "checkinItem",
+            notes = "Checkin Item Request from Owning institution", nickname = "checkinItem")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "OK")})
+    @ResponseBody
+    public ItemResponseInformation checkinItemRequest(@ApiParam(value = "Parameters for requesting an item" , required = true , name = "requestItemJson")@RequestBody ItemRequestInformation itemRequestInfo){
+        ItemResponseInformation itemResponseInformation= null;
+        String response = "";
+        RestTemplate restTemplate = new RestTemplate();
+        try {
+            response = restTemplate.postForEntity(serverProtocol + scsbCircUrl + "requestItem/checkinItem", itemRequestInfo, String.class).getBody();
+            ObjectMapper om = new ObjectMapper();
+            itemResponseInformation = om.readValue(response, ItemResponseInformation.class);
+        }catch(RestClientException ex){
+            logger.error("RestClient : "+ ex.getMessage());
+        }catch(Exception ex){
+            logger.error("Exception : "+ex.getMessage());
+        }
+        return itemResponseInformation;
+    }
+
+    @RequestMapping(value = "/holdItem" , method = RequestMethod.POST)
+    @ApiOperation(value = "holdItem",
+            notes = "hold Item Request from Owning institution", nickname = "holdItem")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "OK")})
+    @ResponseBody
+    public ItemResponseInformation holdItemRequest(@ApiParam(value = "Parameters for requesting an item" , required = true , name = "requestItemJson")@RequestBody ItemRequestInformation itemRequestInfo){
+        ItemResponseInformation itemResponseInformation= null;
+        String response = "";
+        RestTemplate restTemplate = new RestTemplate();
+        try {
+            response = restTemplate.postForEntity(serverProtocol + scsbCircUrl + "requestItem/holdItem", itemRequestInfo, String.class).getBody();
+            ObjectMapper om = new ObjectMapper();
+            itemResponseInformation = om.readValue(response, ItemResponseInformation.class);
+        }catch(RestClientException ex){
+            logger.error("RestClient : "+ ex.getMessage());
+        }catch(Exception ex){
+            logger.error("Exception : "+ex.getMessage());
+        }
+        return itemResponseInformation;
+    }
+
+    @RequestMapping(value = "/cancelHoldItem" , method = RequestMethod.POST)
+    @ApiOperation(value = "cancelHoldItem",
+            notes = "Cancel hold Item Request from Owning institution", nickname = "cancelHoldItem")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "OK")})
+    @ResponseBody
+    public ItemResponseInformation cancelHoldItemRequest(@ApiParam(value = "Parameters for requesting an item" , required = true , name = "requestItemJson")@RequestBody ItemRequestInformation itemRequestInfo){
+        ItemResponseInformation itemResponseInformation= null;
+        String response = "";
+        RestTemplate restTemplate = new RestTemplate();
+        try {
+            response = restTemplate.postForEntity(serverProtocol + scsbCircUrl + "requestItem/cancelHoldItem", itemRequestInfo, String.class).getBody();
+            ObjectMapper om = new ObjectMapper();
+            itemResponseInformation = om.readValue(response, ItemResponseInformation.class);
+        }catch(RestClientException ex){
+            logger.error("RestClient : "+ ex.getMessage());
+        }catch(Exception ex){
+            logger.error("Exception : "+ex.getMessage());
+        }
+        return itemResponseInformation;
+    }
+
+    @RequestMapping(value = "/createBib" , method = RequestMethod.POST)
+    @ApiOperation(value = "createBib",
+            notes = "Breate Bibliographic Request from Owning institution", nickname = "createBib")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "OK")})
+    @ResponseBody
+    public ItemResponseInformation createBibRequest(@ApiParam(value = "Parameters for requesting an item" , required = true , name = "requestItemJson")@RequestBody ItemRequestInformation itemRequestInfo){
+        ItemResponseInformation itemResponseInformation= null;
+        String response = "";
+        RestTemplate restTemplate = new RestTemplate();
+        try {
+            response = restTemplate.postForEntity(serverProtocol + scsbCircUrl + "requestItem/createBib", itemRequestInfo, String.class).getBody();
+            ObjectMapper om = new ObjectMapper();
+            itemResponseInformation = om.readValue(response, ItemResponseInformation.class);
+        }catch(RestClientException ex){
+            logger.error("RestClient : "+ ex.getMessage());
+        }catch(Exception ex){
+            logger.error("Exception : "+ex.getMessage());
+        }
+        return itemResponseInformation;
+    }
     private HttpHeaders getHttpHeaders() {
         HttpHeaders responseHeaders = new HttpHeaders();
         responseHeaders.add(ReCAPConstants.RESPONSE_DATE, new Date().toString());
